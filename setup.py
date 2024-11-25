36bd8f42c692787171149a34a738a087ce8a0a22
Adriano Rutz <adriano.rutz@hotmail.com>
2024-11-25 11:02:53 +0100

    up

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ import subprocess
 from distutils.version import LooseVersion
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
-from setuptools.command.test import test as TestCommand
 from shutil import copyfile, copymode
 
 
@@ -112,6 +111,5 @@ setup(
     package_dir={"": "src"},
     ext_modules=[CMakeExtension("_tmap")],
     cmdclass=dict(build_ext=CMakeBuild),
-    test_suite="tests",
     zip_safe=False,
 )
